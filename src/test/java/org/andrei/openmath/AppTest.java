5efc3d603b875884a9309dec7df641682e46cfdf
Trinitas <Trinitas@Trinitas-PC>
2015-07-25 10:45:34 +0200

    first

--- src/test/java/org/andrei/openmath/AppTest.java
+++ src/test/java/org/andrei/openmath/AppTest.java
@@ -4,37 +4,45 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import org.andrei.entity.AppUser;
+
+public class AppTest
+        extends TestCase {
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
-        assertTrue( true );
+    public void testApp() {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        AppUser user = new AppUser("firstuser");
+        session.save(user);
+
+        session.getTransaction().commit();
+        session.close();
 
     }
 }
