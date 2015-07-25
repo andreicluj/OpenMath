c2efe22ac803dd45ccb8ec7c7da7e487c44df368
Trinitas <Trinitas@Trinitas-PC>
2015-07-25 12:10:33 +0200

    first

--- src/main/java/org/andrei/openmath/App.java
+++ src/main/java/org/andrei/openmath/App.java
@@ -1,17 +1,73 @@
 package org.andrei.openmath;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import net.sourceforge.jeuclid.MathMLParserSupport;
+import net.sourceforge.jeuclid.MutableLayoutContext;
+import net.sourceforge.jeuclid.context.LayoutContextImpl;
+import net.sourceforge.jeuclid.context.Parameter;
+import net.sourceforge.jeuclid.converter.Converter;
+import net.sourceforge.jeuclid.converter.ConverterPlugin.DocumentWithDimension;
+import net.sourceforge.jeuclid.converter.ConverterRegistry;
+import net.sourceforge.jeuclid.elements.generic.DocumentElement;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGDocument;
+
+import uk.ac.ed.ph.snuggletex.SnuggleInput;
+import uk.ac.ed.ph.snuggletex.SnuggleEngine;
+import uk.ac.ed.ph.snuggletex.SnuggleSession;
+import java.io.IOException;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
+public class App {
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("FrameDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(355, 355));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) throws IOException {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+
+        /* Create vanilla SnuggleEngine and new SnuggleSession */
+        SnuggleEngine engine = new SnuggleEngine();
+        SnuggleSession session = engine.createSession();
+
+        /* Parse some very basic Math Mode input */
+        SnuggleInput input = new SnuggleInput("$$ x+2=3 $$");
+        session.parseInput(input);
+
+        /* Convert the results to an XML String, which in this case will
+         * be a single MathML <math>...</math> element. */
+        String xmlString = session.buildXMLString();
+        System.out.println("Input " + input.getString()
+                + " was converted to:\n" + xmlString);
 
     }
+    
+    
+    
+    
 }
--- src/test/java/org/andrei/openmath/AppTest.java
+++ src/test/java/org/andrei/openmath/AppTest.java
@@ -3,12 +3,28 @@ package org.andrei.openmath;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import net.sourceforge.jeuclid.MathMLParserSupport;
+import net.sourceforge.jeuclid.MutableLayoutContext;
+import net.sourceforge.jeuclid.context.LayoutContextImpl;
+import net.sourceforge.jeuclid.context.Parameter;
+import net.sourceforge.jeuclid.converter.Converter;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import org.andrei.entity.AppUser;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGDocument;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import org.junit.Assert;
+//import org.junit.Test;
 
 public class AppTest
         extends TestCase {
@@ -45,4 +61,46 @@ public class AppTest
         session.close();
 
     }
+
+    public static final String TEST1 = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><math mode=\"display\">"
+            + "<mrow><munderover><mo>&#x0222B;</mo><mn>1</mn><mi>x</mi></munderover>"
+            + "<mfrac><mi>dt</mi><mi>t</mi></mfrac></mrow></math>";
+
+    public void testConverterXXX(final String ext, final String mimeext)
+            throws Exception {
+        final Document doc = MathMLParserSupport
+                .parseString(TEST1);
+        final File outFile = new File(this.getOutDir(), "test1." + ext);
+        final MutableLayoutContext params = new LayoutContextImpl(
+                LayoutContextImpl.getDefaultLayoutContext());
+        params.setParameter(Parameter.MATHSIZE, 25f);
+
+        Converter.getInstance().convert(doc, outFile, "image/" + mimeext,
+                params);
+        Assert.assertTrue(outFile.exists());
+        Assert.assertTrue(outFile.length() > 0);
+    }
+
+    public File getOutDir() {
+        final File outDir = new File("temp");
+        if (!outDir.isDirectory()) {
+            final boolean success = outDir.mkdirs();
+            Assert
+                    .assertTrue(
+                            "Failed to create temp directory. Please delete all files / directories named temp",
+                            success);
+        }
+        return outDir;
+    }
+
+    /**
+     * Tests if JPEG converter is available.
+     *
+     * @throws Exception if the test fails.
+     */
+
+    public void testConverterJPG() throws Exception {
+        this.testConverterXXX("jpg", "jpeg");
+    }
+
 }
